8a1b211de19a85ec0b63434fdf9dd01d3d0cdd5f
Enobong-kingsley <enobongndedde@gmail.com>
2023-06-01 00:06:57 +0100

    Added Post Feature

--- lib/widgets/post_tile.dart
+++ lib/widgets/post_tile.dart
@@ -1,8 +1,31 @@
+import 'package:blogapp/widgets/custom_image.dart';
+import 'package:blogapp/widgets/post.dart';
 import 'package:flutter/material.dart';
 
+import '../pages/post_screen.dart';
+
 class PostTile extends StatelessWidget {
+  final Post post;
+
+  PostTile(this.post);
+
+  showPost(context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => PostScreen(
+          postId: post.postId,
+          userId: post.ownerId,
+        ),
+      ),
+    );
+  }  
+
   @override
   Widget build(BuildContext context) {
-    return Text("Post Tile");
+    return GestureDetector(
+      onTap: () => showPost(context),
+      child: cachedNetworkImage(post.mediaUrl),
+    );
   }
 }
